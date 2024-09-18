37652a1baca968bf833e4ad1d8c9ad0707ccd452
Nasvan K <122220975+nasvank7@users.noreply.github.com>
2024-09-18 16:11:02 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,10 @@ import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
 import Navbar from '@/Components/Layout/Navbar/Navbar'
 
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import RootState from '@/services/redux/Store/RootState'
+import { useSelector } from 'react-redux'
 function MyApp({ Component, pageProps }: AppProps) {
   const [mounted, setMounted] = useState(false)
 
@@ -18,6 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <div className="min-h-screen bg-gray-900 text-white">
+      
       <Header />
       <div className="flex flex-col md:flex-row max-w-7xl mx-auto px-4 py-8">
         <div className="hidden md:block md:w-16 lg:w-64 mr-4">
@@ -25,6 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         </div>
         <main className="flex-grow">
           <Component {...pageProps} />
+          <ToastContainer />
         </main>
       </div>
       <div className="md:hidden fixed bottom-0 left-0 right-0 bg-gray-800 z-50">
